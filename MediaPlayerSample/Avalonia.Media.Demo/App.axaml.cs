51f6c058b46bc2a1747120d73d37a1bac4f2b831
Max Katz <maxkatz6@outlook.com>
2025-04-06 20:41:09 -0700

    Replace references with new devtools

--- MediaPlayerSample/Avalonia.Media.Demo/App.axaml.cs
+++ MediaPlayerSample/Avalonia.Media.Demo/App.axaml.cs
@@ -15,6 +15,7 @@ public partial class App : Application
     public override void Initialize()
     {
         AvaloniaXamlLoader.Load(this);
+        this.AttachDeveloperTools();
     }
 
     public override void OnFrameworkInitializationCompleted()
