5c821e096230e861051179ecd6a786e4b31c3d8e
Max Katz <maxkatz6@outlook.com>
2025-04-06 20:26:08 -0700

    Add NativeWebViewAndDialogSample samples

---
+++ NativeWebViewAndDialogSample/NativeWebViewAndDialogSample/ViewModels/MainViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using Avalonia.Controls;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace NativeWebViewAndDialogSample.ViewModels;
+
+public partial class MainViewModel : ObservableObject
+{
+    public MainViewModel()
+    {
+        Url = "https://docs.avaloniaui.net/accelerate/components/webview/nativewebview/";
+        NavigationUrl = new Uri(Url);
+    }
+
+    [ObservableProperty]
+    public partial string Url { get; set; }
+
+    [ObservableProperty]
+    public partial Uri NavigationUrl { get; set; }
+
+    public ObservableCollection<string> Log { get; } = [];
+
+    [RelayCommand]
+    private void Navigate()
+    {
+        try
+        {
+            NavigationUrl = new Uri(Url);
+        }
+        catch (Exception ex)
+        {
+            Log.Add(ex.Message);
+        }
+    }
+
+    [RelayCommand]
+    private void NavigateDialog()
+    {
+        try
+        {
+            var url = new Uri(Url);
+            var dialog = new NativeWebDialog
+            {
+                Source = url
+            };
+            dialog.Resize(400, 500);
+            dialog.Show();
+        }
+        catch (Exception ex)
+        {
+            Log.Add(ex.Message);
+        }
+    }
+}
